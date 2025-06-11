aa00fa8e26efd80bf266dfc11b395dc6ca0f3039
Fabian Silva <98433285+silvafab1981@users.noreply.github.com>
2025-06-11 16:20:34 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
->>>>>>> b147f74 (Primera versión estilo Heisen)
