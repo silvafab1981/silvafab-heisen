1d9bcb96420639c0d37e83bde71cafeb414e5bc8
Fabian Silva <98433285+silvafab1981@users.noreply.github.com>
2025-06-11 16:19:08 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
-import { Hero } from '../templates/Hero';
-import { About } from '../templates/About';
-import { Services } from '../templates/Services';
-import { Portfolio } from '../templates/Portfolio';
-import { Contact } from '../templates/Contact';
-import { Footer } from '../components/Footer';
-=======
 import Head from 'next/head';
 import Hero from '../templates/Hero';
 import About from '../templates/About';
 import Services from '../templates/Services';
+import Portfolio from '../templates/Portfolio';
+import Contact from '../templates/Contact';
 import Footer from '../components/Footer';
->>>>>>> b147f74 (Primera versión estilo Heisen)
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
-      <Hero />
-      <About />
-      <Services />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </>
-  );
-}
-=======
       <Head>
         <title>Silvafab – Infraestructura IT</title>
         <meta name="description" content="Infraestructura, DevOps, Automatización y más." />
@@ -41,4 +23,3 @@ export default function Home() {
     </>
   );
 }
->>>>>>> b147f74 (Primera versión estilo Heisen)
